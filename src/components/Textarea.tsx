a8e8cefa44e5a41d80e804952f2c66777419aa2f
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-10 01:49:08 +0700

    benerin type & props lagi

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -2,14 +2,14 @@ import { forwardRef, useId } from "react";
 
 type TextareaProps = {
   label: string;
-  placeholder?: string;
   baris: number;
-  className: string;
+  placeholder?: string;
+  resize?: boolean;
   value?: string;
   readonly?: boolean;
 };
 
-const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(({ label, placeholder, baris, className, value, readonly = false }, ref) => {
+const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(({ label, placeholder, baris, value, readonly = false, resize = true }, ref) => {
   const id = useId();
 
   return (
@@ -17,7 +17,15 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(({ label, placeh
       <label htmlFor={id} className="text-lg font-semibold">
         {label}
       </label>
-      <textarea placeholder={placeholder} value={value} id={id} rows={baris} ref={ref} className={className} readOnly={readonly}></textarea>
+      <textarea
+        placeholder={placeholder}
+        value={value}
+        id={id}
+        rows={baris}
+        ref={ref}
+        className={`form-textarea mt-0.5 box-border w-full ${resize ? "resize-y" : "resize-none"} rounded border-black px-1 py-0.5 focus:border-transparent focus:ring-1 focus:ring-black`}
+        readOnly={readonly}
+      ></textarea>
     </div>
   );
 });
