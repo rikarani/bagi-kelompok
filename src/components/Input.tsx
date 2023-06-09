fdea691cc1ae6b444776d2e09fc67b351dc3a40e
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-10 01:35:15 +0700

    benerin type & props lagi

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,17 @@
 import { forwardRef } from "react";
 
 type InputProps = {
-  labelText: string;
-  placeholderText: string;
+  label: string;
+  placeholder: string;
 };
 
-const Input = forwardRef<HTMLInputElement, InputProps>(({ labelText, placeholderText }, ref) => {
+const Input = forwardRef<HTMLInputElement, InputProps>(({ label, placeholder }, ref) => {
   return (
     <div>
       <label htmlFor="inputBanyakAnggota" className="text-lg font-semibold">
-        {labelText}
+        {label}
       </label>
-      <input placeholder={placeholderText} className="form-input mb-2 box-border w-full resize-y rounded border-black px-1 py-0.5 focus:border-transparent focus:ring-1 focus:ring-black" ref={ref} id="inputBanyakAnggota" type="number" />
+      <input placeholder={placeholder} className="form-input mb-2 box-border w-full resize-y rounded border-black px-1 py-0.5 focus:border-transparent focus:ring-1 focus:ring-black" ref={ref} id="inputBanyakAnggota" type="number" />
     </div>
   );
 });
--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,22 +1,23 @@
 import { forwardRef, useId } from "react";
 
 type TextareaProps = {
-  labelText: string;
+  label: string;
+  placeholder: string;
   baris: number;
   className: string;
-  innerText?: string;
+  value?: string;
   readonly?: boolean;
 };
 
-const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(({ baris, labelText, className, innerText, readonly = false }, ref) => {
+const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(({ label, placeholder, baris, className, value, readonly = false }, ref) => {
   const id = useId();
 
   return (
     <div>
       <label htmlFor={id} className="text-lg font-semibold">
-        {labelText}
+        {label}
       </label>
-      <textarea value={innerText} id={id} rows={baris} ref={ref} className={className} readOnly={readonly}></textarea>
+      <textarea placeholder={placeholder} value={value} id={id} rows={baris} ref={ref} className={className} readOnly={readonly}></textarea>
     </div>
   );
 });
