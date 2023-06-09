176a6287552f8287180734b33d6cd8acaea18acd
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-10 01:15:17 +0700

    nambahin props onClick

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,13 @@
+import React from "react";
+
 type ButtonProps = {
   text: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-function Button({ text }: ButtonProps) {
+function Button({ text, onClick }: ButtonProps) {
   return (
-    <button onClick={() => alert("Selamat, Aplikasi Gagal")} className="rounded-lg bg-cyan-300 px-4 py-2 hover:bg-cyan-400">
+    <button onClick={onClick} className="rounded-lg bg-slate-500 px-4 py-2 text-slate-100 hover:bg-slate-600">
       {text}
     </button>
   );
