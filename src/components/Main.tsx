c1ccef55cb385439d9750c3ee791d000ec8b4252
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-11 02:09:03 +0700

    ngasih responsive, perubahan gedelah pokoknya

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,14 @@
 import React, { useRef } from "react";
-import Usage from "./Usage";
 import Textarea from "../partials/Textarea";
 import Input from "../partials/Input";
 import Button from "../partials/Button";
+import Hasil from "./Hasil";
+import Summary from "./Summary";
+
+// type Test = never[] | string[] | string[][] | string | undefined;
 
 function Main(): React.JSX.Element {
+  // const [data, setData] = useState<Test>([]);
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -17,44 +21,31 @@ function Main(): React.JSX.Element {
   ];
 
   return (
-    <div className="w-full divide-y-2 divide-black px-2 py-1">
-      {/* Cara Pakai */}
-      <Usage />
-      {/* Cara Pakai */}
+    <div className="divide-y-2 divide-black rounded bg-slate-500/40 px-2 py-1">
+      <div className="mb-1 divide-y-2 divide-black">
+        {/* Inputan */}
+        <div className="w-full">
+          <Textarea ref={textareaRef} baris={5} label="Masukkan Nama Semua Orang" placeholder="cth: Adit, Ujang, Ucok, Ucup, Budi" />
 
-      <div>
-        <div className="mt-2 flex divide-x-2 divide-black rounded bg-slate-500/40 px-2 py-1">
-          {/* Inputan */}
-          <div className="mr-2 w-1/2">
-            <div className=" flex w-full flex-col">
-              <Textarea baris={5} label="Masukkan Nama Semua Orang" placeholder="cth: Adit, Ujang, Ucok, Ucup, Budi" ref={textareaRef} />
-
-              <Input ref={inputRef} label="Jumlah Anggota per Kelompok" placeholder="cth: 3" />
-              <Button onClick={() => alert("Selamat, Aplikasi Gagal")} text="gaskan" />
-            </div>
+          <div className="sm:flex sm:gap-2">
+            <Input ref={inputRef} label="Jumlah Anggota per Kelompok" placeholder="cth: 3" />
+            <Button onClick={() => alert("Selamat, Aplikasi Gagal")} text="Test" />
           </div>
-          {/* Inputan */}
-
-          {/* Hasil */}
-          <div>
-            <div className="ml-2">
-              <h6 className="text-lg font-semibold">Hasil Pembagian Kelompok</h6>
-              <div className="grid grid-cols-3 gap-2">
-                {testData.map((data: string[], index: number) => {
-                  const member: string = data.join(" ").replace(/\s+/g, "\n");
-
-                  return (
-                    <div key={index}>
-                      <Textarea value={member} label={`Kelompok ${index + 1}`} baris={3} readonly={true} resize={false} />
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-          {/* Hasil */}
         </div>
+        {/* Inputan */}
+
+        {/* Hasil */}
+        <div>
+          <Hasil sourceData={testData} />
+        </div>
+        {/* Hasil */}
       </div>
+
+      {/* Summary */}
+      <div>
+        <Summary sourceData={testData} />
+      </div>
+      {/* Summary */}
     </div>
   );
 }
