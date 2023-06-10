f57e45307cca18918cdaec92fab24f7d68be2b1c
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-11 02:07:44 +0700

    pindahin bagian Usage ke Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { IconContext } from "react-icons/lib/esm/iconContext";
 import { BsGithub } from "react-icons/bs";
+import Usage from "./Usage";
 
 function Header(): React.JSX.Element {
   return (
-    <div className="flex items-center gap-2 px-2 py-1">
-      <IconContext.Provider value={{ size: "3.5rem" }}>
-        <a href="https://github.com/rikarani/bagi-kelompok" target="_blank">
-          <BsGithub />
-        </a>
-      </IconContext.Provider>
-      <div>
-        <h1 className="text-2xl font-semibold">Bagi Kelompok</h1>
-        <h2>Projek UAS Mata Kuliah Probabilitas dan Statistik</h2>
+    <div className="px-2 py-1">
+      <div className="flex items-center gap-2">
+        <IconContext.Provider value={{ size: "3.5rem" }}>
+          <a href="https://github.com/rikarani/bagi-kelompok" target="_blank">
+            <BsGithub />
+          </a>
+        </IconContext.Provider>
+        <div>
+          <h1 className="text-2xl font-semibold">Bagi Kelompok</h1>
+          <h2>Projek UAS Mata Kuliah Probabilitas dan Statistik</h2>
+        </div>
+      </div>
+      <div className="mt-2">
+        <Usage />
       </div>
     </div>
   );
