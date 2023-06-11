41cb4378cbf1570cec6e60c8c02d8483fc3af0fa
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-12 00:37:33 +0700

    context consumer

--- src/components/Hasil.tsx
+++ src/components/Hasil.tsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { KelompokContext } from "../context/contexts";
 import Textarea from "../partials/Textarea";
-
 interface HasilProps {
   sourceData: string[][];
 }
 
 function Hasil({ sourceData }: HasilProps): React.JSX.Element {
+  const { dataKelompok } = useContext(KelompokContext);
+
+  const baris: number = dataKelompok[0]?.length;
+
   return (
-    <div className="sm:ml-2">
+    <>
       <h6 className="text-lg font-semibold">Hasil Pembagian Kelompok</h6>
       <div className="grid grid-cols-3 gap-2">
         {sourceData.map((data: string[], index: number) => {
           const member: string = data.join(" ").replace(/\s+/g, "\n");
 
-          return <Textarea key={index} value={member} label={`Kelompok ${index + 1}`} baris={3} readonly={true} resize={false} />;
+          return <Textarea key={index} value={member} label={`Kelompok ${index + 1}`} baris={baris} readonly={true} resize={false} />;
         })}
       </div>
-    </div>
+    </>
   );
 }
 export default Hasil;
