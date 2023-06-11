604f69a13b9b6bb4ef93b73961136a8da9051cf0
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-12 00:36:44 +0700

    context provider

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,51 +1,40 @@
-import React, { useRef } from "react";
-import Textarea from "../partials/Textarea";
-import Input from "../partials/Input";
-import Button from "../partials/Button";
+import React, { useState } from "react";
+import { KelompokContext, CombinationContext } from "../context/contexts";
+import { useIsFirstRender } from "usehooks-ts";
 import Hasil from "./Hasil";
 import Summary from "./Summary";
-
-// type Test = never[] | string[] | string[][] | string | undefined;
+import Inputan from "./Inputan";
 
 function Main(): React.JSX.Element {
-  // const [data, setData] = useState<Test>([]);
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const testData: string[][] = [
-    ["Andi", "Ikbal", "Furqon"],
-    ["Jaka", "Adi", "Baskara"],
-    ["Ucok", "Ali", "Baba"],
-    ["Kurniawan", "Ahmad", "Edo"],
-    ["Ica", "Tasya", "Rizki"],
-  ];
+  const firstRender: boolean = useIsFirstRender();
+  const [dataKelompok, setDataKelompok] = useState<never[] | string[][]>([]);
+  const [dataKombinasi, setDataKombinasi] = useState<never[] | string[][]>([]);
 
   return (
     <div className="divide-y-2 divide-black rounded bg-slate-500/40 px-2 py-1">
-      <div className="mb-1 divide-y-2 divide-black">
-        {/* Inputan */}
-        <div className="w-full">
-          <Textarea ref={textareaRef} baris={5} label="Masukkan Nama Semua Orang" placeholder="cth: Adit, Ujang, Ucok, Ucup, Budi" />
+      <CombinationContext.Provider value={{ dataKombinasi, setDataKombinasi }}>
+        <KelompokContext.Provider value={{ dataKelompok, setDataKelompok }}>
+          <div className="mb-1 divide-y-2 divide-black">
+            {/* Inputan */}
 
-          <div className="sm:flex sm:gap-2">
-            <Input ref={inputRef} label="Jumlah Anggota per Kelompok" placeholder="cth: 3" />
-            <Button onClick={() => alert("Selamat, Aplikasi Gagal")} text="Test" />
+            <Inputan />
+
+            {/* Inputan */}
+
+            {/* Hasil */}
+            <div className={firstRender ? "hidden" : "block"}>
+              <Hasil sourceData={dataKelompok} />
+            </div>
+            {/* Hasil */}
           </div>
-        </div>
-        {/* Inputan */}
 
-        {/* Hasil */}
-        <div>
-          <Hasil sourceData={testData} />
-        </div>
-        {/* Hasil */}
-      </div>
-
-      {/* Summary */}
-      <div>
-        <Summary sourceData={testData} />
-      </div>
-      {/* Summary */}
+          {/* Summary */}
+          <div className={firstRender ? "hidden" : "block"}>
+            <Summary />
+          </div>
+          {/* Summary */}
+        </KelompokContext.Provider>
+      </CombinationContext.Provider>
     </div>
   );
 }
