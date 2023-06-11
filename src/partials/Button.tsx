59780b6a31b3eedc82baf1a033acfcdf8193c295
rikarani <46773591+rikarani@users.noreply.github.com>
2023-06-12 00:32:12 +0700

    import type & sesuaikan border radius

--- src/partials/Button.tsx
+++ src/partials/Button.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-
-interface ButtonProps {
-  text: string;
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
-}
+import { ButtonProps } from "../interfaces/interfaces";
 
 function Button({ text, onClick }: ButtonProps): React.JSX.Element {
   return (
-    <button onClick={onClick} className="mb-2 w-full self-end rounded-lg bg-slate-500 px-4 py-2 text-slate-100 hover:bg-slate-600 sm:w-1/2">
+    <button onClick={onClick} className="mb-2 w-full self-end rounded bg-slate-500 px-4 py-2 text-slate-100 hover:bg-slate-600 sm:w-1/2">
       {text}
     </button>
   );
